a80e5df1ce071095b4fe871cee60497e8a0bfc66
“Takabachi <bn.hachigo@gmail.com>
2025-05-17 13:43:51 +0900

    commit5: next.config.mjs images

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,11 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',                // 静的エクスポートを有効化
   trailingSlash: true,             // 末尾に / を付与（GitHub Pages 404 防止）
-  images: { unoptimized: true },   // 画像最適化をオフ（export 対応）
+  images: {
+    unoptimized: true,       // 最適化オフ
+    loader: 'custom',        // カスタムローダーを指定
+    loaderFile: './image-loader.js', 
+  },
   // GitHub Pages 用のパスプレフィクス
   basePath: isProd ? '/prime_reform_gym' : '',
   assetPrefix: isProd ? '/prime_reform_gym/' : '',
