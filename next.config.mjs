dd963b38bb1d641e492d27688639eca807004d9c
“Takabachi <bn.hachigo@gmail.com>
2025-05-17 13:20:11 +0900

    second: 設定ファイル修正

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,8 @@ const nextConfig = {
   trailingSlash: true,             // 末尾に / を付与（GitHub Pages 404 防止）
   images: { unoptimized: true },   // 画像最適化をオフ（export 対応）
   // GitHub Pages 用のパスプレフィクス
-  basePath: isProd ? '/https://github.com/bn-hashi/prime_reform_gym.git' : '',
-  assetPrefix: isProd ? '/https://github.com/bn-hashi/prime_reform_gym.git/' : '',
+  basePath: isProd ? '/prime_reform_gym' : '',
+  assetPrefix: isProd ? '/prime_reform_gym/' : '',
 };
 
 export default nextConfig;
