bbba69c29433b5973d846947c55e6c2105d82b63
“Takabachi <bn.hachigo@gmail.com>
2025-05-17 13:51:22 +0900

    commit6: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,9 @@ const nextConfig = {
   trailingSlash: true,             // 末尾に / を付与（GitHub Pages 404 防止）
   images: {
     unoptimized: true,       // 最適化オフ
-    loader: 'custom',        // カスタムローダーを指定
-    loaderFile: './image-loader.js', 
   },
   // GitHub Pages 用のパスプレフィクス
-  basePath: isProd ? '/prime_reform_gym' : '',
-  assetPrefix: isProd ? '/prime_reform_gym/' : '',
+  assetPrefix: isProd ? './' : '',
 };
 
 export default nextConfig;
